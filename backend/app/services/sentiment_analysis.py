1ae2ef581d737f84699c8e175e38482c49975a13
atmihaa_06 <atmihaa@gmail.com>
2026-06-28 19:20:39 +0530

    Lazy load sentiment model

--- backend/app/services/sentiment_analysis.py
+++ backend/app/services/sentiment_analysis.py
@@ -1,14 +1,24 @@
 from transformers import pipeline
 
+classifier = None
+
+def get_classifier():
+    global classifier
+
+    if classifier is None:
+        classifier = pipeline(
+            "sentiment-analysis",
+            model="distilbert-base-uncased-finetuned-sst-2-english",
+            framework="pt"
+        )
+
+    return classifier
+
+
 def analyze_sentiment(text):
+    model = get_classifier()
 
-    classifier = pipeline(
-        "sentiment-analysis",
-        model="distilbert-base-uncased-finetuned-sst-2-english",
-        framework="pt"
-    )
-
-    result = classifier(text[:512])
+    result = model(text[:512])
 
     return {
         "label": result[0]["label"],
